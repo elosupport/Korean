df75ae9f64e83879adcb1e777d44de84b31c9a4d
elosupport <krxeto@gmail.com>
2016-02-12 09:02:11 +0900

    6.3 fix
    6.3 fix

--- LittleHumanizer/Properties/AssemblyInfo.cs
+++ LittleHumanizer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Blitzcrank")]
+[assembly: AssemblyTitle("LittleHumanizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blitzcrank")]
-[assembly: AssemblyCopyright("Copyright © Hellsing 2015")]
+[assembly: AssemblyProduct("LittleHumanizer")]
+[assembly: AssemblyCopyright("EloSupport")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2016.02.12.1")]
+[assembly: AssemblyFileVersion("2016.02.12.1")]
