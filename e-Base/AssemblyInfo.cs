51e9fda609b74c518d69c74ed13df90142a61ec0
elosupport <krxeto@gmail.com>
2016-04-26 19:52:40 +0900

    eBase
    eBase

--- e-Base/AssemblyInfo.cs
+++ e-Base/AssemblyInfo.cs
@@ -25,11 +25,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Evade Fixed")]
+[assembly: AssemblyTitle("eBase")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Evade Fixed")]
+[assembly: AssemblyProduct("eBase")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2016.02.22.1")]
-[assembly: AssemblyFileVersion("2016.02.22.1")]
+[assembly: AssemblyVersion("2016.04.26.1")]
+[assembly: AssemblyFileVersion("2016.04.26.1")]
